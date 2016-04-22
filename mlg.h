b468ebdccd6fc5fae7d72c8930d5a1d80f882b15
TheMonsterFromTheDeep <TheMonsterFromTheDeep@users.noreply.github.com>
2016-04-21 21:13:50 -0600

    Quickscope mlg.h, test.cpp
    mlg.h = dank memes & stuff
    test.cpp = actual gameplay lol

---
+++ mlg.h
@@ -0,0 +1,42 @@
+#include <cstdio>
+
+#define ILLUMINATI (3)
+#define CONFIRMED * (1)
+#define BEGINS {
+#define SHREKT }
+#define HAX *
+#define MEME int
+#define SAMPLE
+#define TEXT char
+#define DANK const
+#define GET return
+#define REKT 0
+#define PWNAGE int main(MEME MEMECOUNT, SAMPLE TEXT HAX HAX MEMES)
+#define LOL ;
+#define NOOB ;
+#define SKRUB ;
+#define IS =
+#define QUICKSCOPE new
+#define NOSCOPE delete
+#define YOLO [
+#define SWAG ]
+#define IF if(
+#define M8 ) {
+#define MTN for(
+#define DEW ;
+#define DORITOS ;
+#define EQUALS ==
+#define LESS <
+#define MORE >
+#define WIN ++
+#define FAIL --
+#define SHOUT printf("%s\n",
+#define COUNT printf("%d\n",
+#define RITE )
+#define THE (*(
+#define ST - 1
+#define ND - 1
+#define RD - 1
+#define TH - 1
+#define OF +
+#define M9 ))
